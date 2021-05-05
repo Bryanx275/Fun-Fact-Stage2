e593530365135e7db7ff3d9f113cb97293f40990
BryanZee <bryanzee68@gmail.com>
2021-05-05 16:05:35 +0100

    Initial commit

---
+++ app/src/main/java/com/bryncollections/fun_fact/DummyData.kt
@@ -0,0 +1,30 @@
+package com.bryncollections.fun_fact
+
+object DummyData {
+    val funFacts = listOf(
+        FactModel(
+            "Kotlin",
+            R.drawable.kotlin_logo,
+            "The name kotlin comes from Kotlin Island, near St. Petersburg. Andrey Breslay mentioned that the team decided to..."
+        ),
+
+        FactModel(
+            "PromotionChurch",
+            R.drawable.promo_logo,
+            "This is promotion church of God with swagg and love and prayer and good music"
+        ),
+
+        FactModel(
+            "Kotlin",
+            R.drawable.kotlin_logo,
+            "The name kotlin comes from Kotlin Island, near St. Petersburg. Andrey Breslay mentioned that the team decided to..."
+        ),
+
+        FactModel(
+            "Kotlin",
+            R.drawable.kotlin_logo,
+            "The name kotlin comes from Kotlin Island, near St. Petersburg. Andrey Breslay mentioned that the team decided to..."
+        ),
+
+    )
+}
---
+++ app/src/main/java/com/bryncollections/fun_fact/FactModel.kt
@@ -0,0 +1,5 @@
+package com.bryncollections.fun_fact
+
+import android.media.tv.TvContract
+
+data class FactModel(val name: String, val logo: Int, val fact: String)
